da86e089534407f8e1b3c21f8aa5325b953eaa26
Jenish Limbu <jlimbu539@gmail.com>
2023-09-09 19:07:35 +0545

    added static data and reducers

---
+++ src/Data.js
@@ -0,0 +1,18 @@
+export const userList=[{
+    id:1,
+    name:'jenish',
+    email:"jenish1@gmail.com",
+    
+},
+{
+    id:1,
+    name:'Imehang',
+    email:"imsong@gmail.com",
+    
+},
+{
+    id:3,
+    name:'Limbu',
+    email:"limbu4@gmail.com",
+    
+}]
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { Provider } from 'react-redux';
 import App from './App';
-
+import { configureStore } from '@reduxjs/toolkit';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store=configureStore({
+  reducer:{
+    //user //reducer name
+  }
+})
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
